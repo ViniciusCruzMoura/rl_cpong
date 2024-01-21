25cf87a1d78f62b8c00d1729d87875e7ff5c43df
Vinicius Eduardo Carvalho da Cruz de Moura <contato.cruz.moura@gmail.com>
2024-01-21 19:39:27 -0400

    add menu.c

---
+++ src/core/menu.c
@@ -0,0 +1,49 @@
+#include "menu.h"
+#include "game.h"
+
+void init_menu(CMenu *menu) {
+    // Init menu
+    int pos_x = game.screenWidth/2;
+    int pos_y = game.screenHeight/2.5f;
+    char *menu_opts[] = {
+        "FULLSCREEN",
+        "WINDOWED",
+    };
+    menu->optionSelected = 0;
+    menu->mouseHoverRec = -1;
+    for (int i = 0; i < (sizeof(menu_opts) / sizeof(menu_opts[0])); i++) {
+        menu->option[i].label = menu_opts[i];
+        menu->option[i].recs = (Rectangle){ 
+            pos_x - 150.0f/2,
+            pos_y + 32*i,
+            150.0f,
+            30.0f
+        };
+    }
+}
+
+void update_menu(CMenu *menu) {
+    // Mouse toggle group logic of menu options
+    for (int i = 0; i < (sizeof(menu->option) / sizeof(menu->option[0])); i++) {
+        if (CheckCollisionPointRec(GetMousePosition(), menu->option[i].recs))
+        {
+            menu->mouseHoverRec = i;
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+            {
+                menu->optionSelected = i;
+            }
+            break;
+        }
+        else menu->mouseHoverRec = -1;
+    }
+}
+
+void draw_menu(CMenu *menu) {
+    // Draw menu options
+    for (int i = 0; i < (sizeof(menu->option) / sizeof(menu->option[0])); i++) {
+        DrawRectangleRec(menu->option[i].recs, ((i == menu->optionSelected) || (i == menu->mouseHoverRec)) ? SKYBLUE : LIGHTGRAY);
+        DrawRectangleLines((int)menu->option[i].recs.x, (int) menu->option[i].recs.y, (int) menu->option[i].recs.width, (int) menu->option[i].recs.height, ((i == menu->optionSelected) || (i == menu->mouseHoverRec)) ? BLUE : GRAY);
+        DrawText( menu->option[i].label, (int)( menu->option[i].recs.x + menu->option[i].recs.width/2 - MeasureText(menu->option[i].label, 10)/2), (int) menu->option[i].recs.y + 11, 10, ((i == menu->optionSelected) || (i == menu->mouseHoverRec)) ? DARKBLUE : DARKGRAY);
+    }
+}
