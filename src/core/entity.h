46d1349ffb40dd77a830ebb4b66b99fa83276187
Vinicius Eduardo Carvalho da Cruz de Moura <contato.cruz.moura@gmail.com>
2024-01-21 21:39:55 -0400

    remove struct

--- src/core/entity.h
+++ src/core/entity.h
@@ -1,12 +1,6 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-typedef struct CBall {
-    float radius;
-    Vector2 position;
-    Vector2 speed;
-} CBall;
-
 typedef struct CPaddle {
     float speed;
     Rectangle rec;
