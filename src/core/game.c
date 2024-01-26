269409d4df0d29d0bdbc5694e7ba19f2dc64b540
Vinicius Eduardo Carvalho da Cruz de Moura <contato.cruz.moura@gmail.com>
2024-01-26 09:03:04 -0400

    remove duplicated code

--- src/core/game.c
+++ src/core/game.c
@@ -1,4 +1,4 @@
-#include "game.h"
+#include "core\game.h"
 
 void InitGame() 
 {
@@ -31,11 +31,11 @@ void InitGame()
     init_menu(&game.menu, game.screenWidth/2, game.screenHeight/2.5f);
 
     // Textures loading
-    game.texLogo = LoadTexture("resources/raylib_logo.png");
+    game.texLogo = LoadTexture("../resources/raylib_logo.png");
     
     // Sounds loading
     InitAudioDevice();      // Initialize audio device
-    game.soundArray[0] = LoadSound("resources/pong.wav");
+    game.soundArray[0] = LoadSound("../resources/pong.wav");
 
     SetTargetFPS(60);
 }
@@ -46,90 +46,19 @@ void UpdateGame()
     {
         case LOGO: 
         {
-            // Update LOGO screen data here!
-            
-            game.framesCounter++;
-            
-            if (game.framesCounter > 180) 
-            {
-                game.currentScreen = TITLE;    // Change to TITLE screen after 3 seconds
-                game.framesCounter = 0;
-            }
-            
+            update_logo_screen();
         } break;
         case TITLE: 
-        {
-            // Update TITLE screen data here!
-            
-            game.framesCounter++;
-            
-            if (IsKeyPressed(KEY_ENTER)) game.currentScreen = GAMEPLAY;
-
-            update_menu(&game.menu);
-
+        {   
+            update_title_screen();
         } break;
         case GAMEPLAY:
         { 
-            // Update GAMEPLAY screen data here!
-
-            if (IsKeyPressed(KEY_SPACE)) game.gamePaused = !game.gamePaused;
-
-            if (!game.gamePaused && !game.gameOver)
-            {
-                // TODO: Gameplay logic
-
-                update_ball(&game.ball, &game.player, &game.soundArray);
-
-                update_player(&game.player, &game.ball);
-
-                // THE MOUSE IS COLLIDING WITH THE PADDLE
-                game.isMouseOverPaddle = 0;
-                game.isMouseClickingPaddle = 0;
-                if (CheckCollisionPointRec(GetMousePosition(), game.player.paddleLeft.rec)) {
-                    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) game.isMouseClickingPaddle = 1;
-                    else game.isMouseOverPaddle = 1;
-                }
-
-                // THE PLAYER WON THE GAME
-                if (game.ball.position.x < 0) {
-                    strcpy(game.winner, "RIGHT_PADDLE_WIN");
-                    //game.gamePaused = !game.gamePaused;
-                    game.gameOver = true;
-                }
-                else if (game.ball.position.x > GetScreenWidth()) {
-                    strcpy(game.winner, "LEFT_PADDLE_WIN");
-                    //game.gamePaused = !game.gamePaused;
-                    game.gameOver = true;
-                }
-                
-            }
-
-            // RESET GAME
-            if (IsKeyDown(KEY_Q)) {
-                strcpy(game.winner, "");
-                game.currentScreen = LOGO;
-                game.gameOver = false;
-                game.gamePaused = false;  // Game paused state toggle
-                game.elementPositionY = -128;
-                game.framesCounter = 0;  // General pourpose frames counter
-                
-                init_player(&game.player, game.screenWidth - 50, game.screenHeight / 2, game.screenScale);
-                init_ball(&game.ball, game.screenScale);
-            }
-
+            update_gameplay_screen();
         } break;
         case ENDING: 
         {
-            // Update END screen data here!
-            
-            game.framesCounter++;
-            
-            if (IsKeyPressed(KEY_ENTER))
-            {
-                // Replay / Exit game logic
-                game.currentScreen = TITLE;
-            }
-            
+            update_ending_screen();
         } break;
         default: break;
     }
@@ -146,93 +75,19 @@ void DrawGame()
         {
             case LOGO: 
             {
-                // Draw LOGO screen here!
-                
-                DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
-
-                game.elementPositionY += 3;
-                if (game.elementPositionY > (game.screenHeight/3 - 64)) game.elementPositionY = game.screenHeight/3 - 64;
-                DrawTexturePro(
-                    game.texLogo,
-                    (Rectangle){ 0, 0, game.texLogo.width, game.texLogo.height },
-                    (Rectangle){ game.screenWidth/2, game.screenHeight/2, game.texLogo.width, game.texLogo.height },
-                    (Vector2){ game.texLogo.width / 2, game.texLogo.height / 2 - game.elementPositionY /4 },
-                    0,
-                    WHITE
-                );
-                
+                draw_logo_screen();
             } break;
             case TITLE: 
             {
-                // Draw TITLE screen here!
-                
-                DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
-                
-                if ((game.framesCounter/30)%2 == 0) DrawText("PRESS [ENTER] to START", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] to START", 20)/2, GetScreenHeight()/2 + 60, 20, DARKGRAY);
-                
-                draw_menu(&game.menu);
-                
+                draw_title_screen();                
             } break;
             case GAMEPLAY:
-            { 
-                // Draw GAMEPLAY screen here!
-
-                // Draw pause message when required
-                if (game.gamePaused) DrawText("GAME PAUSED", game.screenWidth/2 - MeasureText("GAME PAUSED", 40)/2, game.screenHeight/2 + 60, 40, GRAY);
-
-                if (game.isMouseOverPaddle) ClearBackground(GREEN);
-                if (game.isMouseClickingPaddle) ClearBackground(RED);
-                draw_ball(&game.ball);
-                draw_player(&game.player);
-
-                if (game.gameOver) {
-                    if (strcmp(game.winner, "RIGHT_PADDLE_WIN") == 0) {
-                        char msg[] = "Congrats! The right player win the game!";
-                        DrawText(
-                            msg,
-                            GetScreenWidth()/2.0f - MeasureText(msg, 12*game.screenScale)/2,
-                            GetScreenHeight()/4.0f, 
-                            12*game.screenScale, 
-                            LIGHTGRAY
-                        );
-                        char msg2[] = "PRESS [Q] TO PLAY AGAIN";
-                        DrawText(
-                            msg2, 
-                            GetScreenWidth()/2 - MeasureText(msg2, 20)/2, 
-                            GetScreenHeight()/2 + 180, 
-                            20, 
-                            GRAY
-                        );
-                    }
-                    else if (strcmp(game.winner, "LEFT_PADDLE_WIN") == 0) {
-                        char msg[] = "Congrats! The left player win the game!";
-                        DrawText(
-                            msg,
-                            GetScreenWidth()/2.0f - MeasureText(msg, 12*game.screenScale)/2, 
-                            GetScreenHeight()/4.0f, 
-                            12*game.screenScale, 
-                            LIGHTGRAY
-                        );
-                        char msg2[] = "PRESS [Q] TO PLAY AGAIN";
-                        DrawText(
-                            msg2, 
-                            GetScreenWidth()/2 - MeasureText(msg2, 20)/2, 
-                            GetScreenHeight()/2 + 280, 
-                            20, 
-                            GRAY
-                        );
-                    }
-                }
-                
+            {
+                draw_gameplay_screen();   
             } break;
             case ENDING: 
             {
-                // Draw END screen here!
-                
-                DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
-                
-                if ((game.framesCounter/30)%2 == 0) DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 + 80, 20, GRAY);
-                
+                draw_ending_screen();                
             } break;
             default: break;
         }
