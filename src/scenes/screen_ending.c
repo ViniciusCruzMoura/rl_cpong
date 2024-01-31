1c68a83dbc2497b3727a8ff38a53cdaf157c1457
Vinicius Eduardo Carvalho da Cruz de Moura <contato.cruz.moura@gmail.com>
2024-01-31 01:06:52 -0400

    rename functions

--- src/scenes/screen_ending.c
+++ src/scenes/screen_ending.c
@@ -1,8 +1,8 @@
 #include "game.h"
 
-void init_ending_screen() {}
+void InitEndingScreen() {}
 
-void update_ending_screen() {
+void UpdateEndingScreen() {
     // Update END screen data here!
     game.framesCounter++;
             
@@ -13,10 +13,10 @@ void update_ending_screen() {
     }
 }
 
-void draw_ending_screen() {
+void DrawEndingScreen() {
     // Draw END screen here!
     DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);                
     if ((game.framesCounter/30)%2 == 0) DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 + 80, 20, GRAY);
 }
 
-void unload_ending_screen() {}
+void UnloadEndingScreen() {}
