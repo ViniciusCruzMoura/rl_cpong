061e6644750f38b79740762afa1a871229398291
Vinicius Eduardo Carvalho da Cruz de Moura <contato.cruz.moura@gmail.com>
2024-01-30 22:08:48 -0400

    add const

--- include/ball.h
+++ include/ball.h
@@ -11,6 +11,6 @@ typedef struct CBall {
 
 void init_ball(CBall *ball, int scale);
 void update_ball(CBall *ball, CPlayer *player, Sound *sfx);
-void draw_ball(CBall ball);
+void draw_ball(const CBall ball);
 
 #endif // BALL_H
