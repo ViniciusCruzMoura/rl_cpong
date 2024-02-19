f951c511142662f21a57e8c42b8bbc5cc0eb7d60
Vinicius Eduardo Carvalho da Cruz de Moura <contato.cruz.moura@gmail.com>
2024-02-18 20:41:32 -0400

    change menu func name

--- src/scenes/title.c
+++ src/scenes/title.c
@@ -6,14 +6,16 @@ void UpdateTitleScreen() {
     // Update TITLE screen data here!
     game.framesCounter++;
     if (IsKeyPressed(KEY_ENTER)) game.currentScreen = GAMEPLAY;
-    UpdateMenu(&game.menu);
+    // UpdateMenu(&game.menu);
+    Menu_update(&game.menu);
 }
 
 void DrawTitleScreen() {
     // Draw TITLE screen here!
     DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
     if ((game.framesCounter/30)%2 == 0) DrawText("PRESS [ENTER] to START", GetScreenWidth()/2 - MeasureText("PRESS [ENTER] to START", 20)/2, GetScreenHeight()/2 + 60, 20, DARKGRAY);
-    DrawMenu(game.menu);
+    // DrawMenu(game.menu);
+    Menu_draw(&game.menu);
 }
 
 void UnloadTitleScreen() {}
