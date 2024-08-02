9639a0f5eb2de3846ea702f6d9e9bde11578416a
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-08-02 00:55:17 -0400

    feat: build with docker

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Installation:
 - download and install the raylib 5
 - execute the 'build.bat' file to compile
 - open .exe
+- docker run -it --rm -v "$PWD":/usr/src/app -w /usr/src/app ubuntu:22.04 sh -c "./configure.sh && ./build.sh"
 ```
---
+++ configure.sh
@@ -0,0 +1,8 @@
+apt -y update
+apt -y install build-essential gcc git cmake
+apt -y install libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev libwayland-dev libxkbcommon-dev
+git clone --depth 1 https://github.com/raysan5/raylib.git raylib
+cd raylib/src/
+make PLATFORM=PLATFORM_DESKTOP
+make install
+rm -rf raylib/
