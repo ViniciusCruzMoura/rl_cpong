462499f6c512ceb5e86194ecf6a0d6c77ec13910
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-08-31 17:48:23 -0400

    change(entity): char tag to enum

--- include/entity.h
+++ include/entity.h
@@ -1,10 +1,19 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+typedef enum
+{
+    ENTITY_TYPE_NOTHING = 0,
+    ENTITY_TYPE_OBJECT,
+    ENTITY_TYPE_PLAYER,
+    ENTITY_TYPE_ENEMY,
+    ENTITY_TYPE_ITEM,
+} EntityTypes;
+
 typedef struct Entity {
     bool m_active;
     size_t m_id;
-    char *m_tag;
+    EntityTypes m_tag;
 
     //flags
     CFlags flags;
