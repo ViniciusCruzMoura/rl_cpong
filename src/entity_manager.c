743412664f5f4ed89baf0ba4f909a20fdb409885
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-08-31 17:52:53 -0400

    change(entity_manager): filter by entity type

--- src/entity_manager.c
+++ src/entity_manager.c
@@ -2,29 +2,26 @@
 
 void update_entities(EntityManager *em)
 {
-    if (!em || !em->m_map)
+    if (!em || !em->m_vec)
     {
-        die("fatal: entity_manager/entities_map not initialized\n");
+        die("fatal: entity_manager/entities_vec not initialized\n");
     }
-    Entity * e_list[MAX_ENTITY_MAP_COLUMNS];
-    for (int i=0; em->m_map[ENTITY_TYPE_PLAYER][i]; ++i)
+    for (int i=0; i < MAX_ENTITIES; ++i)
     {
-        e_list[i] = em->m_map[ENTITY_TYPE_PLAYER][i];
-        //printf("Entity %li\n", e_list[i]->m_id);
-        //printf("Entity %s\n", e_list[i]->m_tag);
+        if (!em->m_to_add[i])
+        {
+            continue;
+        }
+        em->m_vec[em->m_to_add[i]->m_id] = em->m_to_add[i];
+        printf("EntityID to ADD %li\n", em->m_to_add[i]->m_id);
     }
+    memset(em->m_to_add, 0, sizeof em->m_to_add);
+    // free not active entities
 }
-/**
-size_t cn = 0;
-for (int i=0; h_map[PLAYER][i]; ++i) {
-    cn++;
-}
-printf("%i", cn);
-*/
 
-Entity *add_entity(EntityManager *em, char * tag, EntityTypes type)
+Entity *add_entity(EntityManager *em, EntityTypes tag)
 {
-    if (em->m_total_entities > MAX_ENTITY_MAP_COLUMNS)
+    if (em->m_total_entities > MAX_ENTITIES)
     {
         die("fatal: entities map, out of index");
     }
@@ -35,14 +32,30 @@ Entity *add_entity(EntityManager *em, char * tag, EntityTypes type)
     e->m_id = ++em->m_total_entities;
     e->m_active = true;
     e->m_tag = tag;
-    em->m_map[type][em->m_total_entities] = e;
+    em->m_to_add[em->m_total_entities] = e;
 
     // Remember to free the allocated memory when done
     // free(e);
     
     printf("Entity ID: %zu : Stored in map at: %p\n",
-            e->m_id, (void*)em->m_map[type][em->m_total_entities]);
+            e->m_id, (void*)em->m_to_add[em->m_total_entities]);
     return e;
 }
 
-
+Entity **get_entities(EntityManager *em, EntityTypes etype)
+{
+    static Entity *new_list[MAX_ENTITIES];
+    for (int i=0; i < MAX_ENTITIES; ++i)
+    {
+        if (!em->m_vec[i])
+        {
+            continue;
+        }
+        if (em->m_vec[i]->m_tag != etype)
+        {
+            continue;
+        }
+        new_list[i] = em->m_vec[i];
+    }
+    return new_list;
+}
