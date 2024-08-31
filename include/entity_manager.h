37e5d183f8e5648511ac1385aa8118df783334cc
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-08-31 17:50:22 -0400

    change(entity_manager): add entities vec and filter by types

--- include/entity_manager.h
+++ include/entity_manager.h
@@ -1,23 +1,19 @@
 #ifndef ENTITY_MANAGER_H
 #define ENTITY_MANAGER_H
 
-#define MAX_ENTITY_MAP_ROW 4
-#define MAX_ENTITY_MAP_COLUMNS 32 * MAX_ENTITY_MAP_ROW * 1000
-
-typedef enum 
-{
-    ENTITY_TYPE_NOTHING = 0,
-    ENTITY_TYPE_OBJECT,
-    ENTITY_TYPE_PLAYER,
-    ENTITY_TYPE_ENEMY,
-} EntityTypes;
+//#define MAX_ENTITY_MAP_ROW 5
+//#define MAX_ENTITY_MAP_COLUMNS 32 * MAX_ENTITY_MAP_ROW
+#define MAX_ENTITIES 128000
 
 typedef struct EntityManager {
     size_t m_total_entities;
-    Entity *m_map[MAX_ENTITY_MAP_ROW][MAX_ENTITY_MAP_COLUMNS];
+    //Entity *m_map[MAX_ENTITY_MAP_ROW][MAX_ENTITY_MAP_COLUMNS];
+    Entity *m_vec[MAX_ENTITIES];
+    Entity *m_to_add[MAX_ENTITIES];
 } EntityManager;
 
 void update_entities(EntityManager *em);
-Entity *add_entity(EntityManager *em, char * tag, EntityTypes type);
+Entity *add_entity(EntityManager *em, EntityTypes tag);
+Entity **get_entities(EntityManager *em, EntityTypes etype);
 
 #endif //ENTITY_MANAGER_H
