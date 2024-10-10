dd1408f10c2f4aa384aaef6c0edaaa187d917fc3
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-10-10 15:50:27 -0400

    change(entity_manager): removed commented code

--- include/entity_manager.h
+++ include/entity_manager.h
@@ -1,13 +1,10 @@
 #ifndef ENTITY_MANAGER_H
 #define ENTITY_MANAGER_H
 
-//#define MAX_ENTITY_MAP_ROW 5
-//#define MAX_ENTITY_MAP_COLUMNS 32 * MAX_ENTITY_MAP_ROW
 #define MAX_ENTITIES 128000
 
 typedef struct EntityManager {
     size_t m_total_entities;
-    //Entity *m_map[MAX_ENTITY_MAP_ROW][MAX_ENTITY_MAP_COLUMNS];
     Entity *m_vec[MAX_ENTITIES];
     Entity *m_to_add[MAX_ENTITIES];
 } EntityManager;
