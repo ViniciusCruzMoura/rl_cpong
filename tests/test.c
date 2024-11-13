0215857a869d89b34d9dc85e2ada2a26c0c3ff45
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-11-13 10:49:02 -0400

    change(test): printf to macro TEST_ASSERT

--- tests/test.c
+++ tests/test.c
@@ -1,15 +1,21 @@
 #include "rlcpong.h"
 
+#define TEST_ASSERT(cond, expected_value) \
+    do { \
+        if (cond) { \
+            printf("PASS| %s(): Expected '%s' - Test Passed\n", __func__, expected_value); \
+        } else { \
+            printf("^^^^^FAIL| %s(): Expected '%s' - Test Failed\n", __func__, expected_value); \
+        } \
+    } while (0)
+
 size_t total_entities = 0;
 
 void test_entity_create()
 {
     Entity *e = malloc(sizeof(Entity));
     entity_create(&e, ENTITY_TYPE_PLAYER, ++total_entities);
-    printf("%s %s(): %s %s %i %s %i \n", 
-            (e->m_tag == ENTITY_TYPE_PLAYER) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "entity tag should be same", 
-            "ENTITY_TAG:", e->m_tag, "TAG:", ENTITY_TYPE_PLAYER);
+    TEST_ASSERT(e->m_tag == ENTITY_TYPE_PLAYER, "entity tag should be same");
     for (size_t i = 0; i < 128000; ++i) {
         entity_create(&e, ENTITY_TYPE_OBJECT, ++total_entities);
     }
@@ -17,9 +23,7 @@ void test_entity_create()
         entity_create(&e, ENTITY_TYPE_ITEM, ++total_entities);
     }
     size_t count = entity_size(e);
-    printf("%s %s(): %s\n", 
-            (count >= 256000) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "should have than 256000 entities"); 
+    TEST_ASSERT(count >= 256000, "should have than 256000 entities");
 }
 
 void test_entity_search_by_tag()
@@ -31,15 +35,10 @@ void test_entity_search_by_tag()
     entity_create(&e, ENTITY_TYPE_PLAYER, ++total_entities);
     
     e = entity_search_by_tag(e, ENTITY_TYPE_PLAYER);
-    printf("%s %s(): %s %s %i %s %i \n", 
-            (e->m_tag == ENTITY_TYPE_PLAYER) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "entity tag should be same", 
-            "ENTITY_TAG:", e->m_tag, "TAG:", ENTITY_TYPE_PLAYER);
+    TEST_ASSERT(e->m_tag == ENTITY_TYPE_PLAYER, "entity tag should be same");
 
     size_t count = entity_size(e);
-    printf("%s %s(): %s\n", 
-            (count == 2) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "should have 2 entities with player tag"); 
+    TEST_ASSERT(count == 2, "should have 2 entities with player tag");
 }
 
 void test_entity_search_by_id()
@@ -51,15 +50,9 @@ void test_entity_search_by_id()
 
     size_t tst_id = entity_create(&e, ENTITY_TYPE_NOTHING, ++total_entities)->m_id;
     e = entity_search_by_id(e, tst_id);
-    printf("%s %s(): %s \n", 
-            (e != NULL) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "shouldn't be NULL pointer");
-    printf("%s %s(): %s \n", 
-            (e->m_id == tst_id) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "entity id should be same");
-    printf("%s %s(): %s \n", 
-            (e->m_tag == ENTITY_TYPE_NOTHING) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "entity tag should be same");
+    TEST_ASSERT(e != NULL, "shouldn't be NULL pointer");
+    TEST_ASSERT(e->m_id == tst_id, "entity id should be same");
+    TEST_ASSERT(e->m_tag == ENTITY_TYPE_NOTHING, "entity tag should be same");
 }
 
 void test_entity_remove_by_id()
@@ -71,9 +64,7 @@ void test_entity_remove_by_id()
     size_t tst_id = entity_create(&e, ENTITY_TYPE_NOTHING, ++total_entities)->m_id;
     entity_remove_by_id(&e, tst_id);
     e = entity_search_by_id(e, tst_id);
-    printf("%s %s(): %s \n", 
-            (e == NULL) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "should be NULL pointer");
+    TEST_ASSERT(e == NULL, "should be NULL pointer");
 }
 
 void test_entity_remove_by_tag()
@@ -88,9 +79,7 @@ void test_entity_remove_by_tag()
     entity_remove_by_tag(&e, ENTITY_TYPE_PLAYER);
     e = entity_search_by_tag(e, ENTITY_TYPE_PLAYER);
     size_t count = entity_size(e);
-    printf("%s %s(): %s\n", 
-            (count == 0) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "should have 0 entities with player tag"); 
+    TEST_ASSERT(count == 0, "should have 0 entities with player tag");
 } 
 
 void test_entity_append()
@@ -113,9 +102,7 @@ void test_entity_append()
     size_t second_el_size = entity_size(e2);
     entity_append(&e, e2);
     size_t end_size = entity_size(e);
-    printf("%s %s(): %s\n", 
-            (first_el_size+second_el_size == end_size) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "should append 2 entities list"); 
+    TEST_ASSERT(first_el_size+second_el_size == end_size, "should append 2 entities list");
 }
 
 void test_ini_file_parser()
@@ -124,9 +111,7 @@ void test_ini_file_parser()
     parse_ini_file("tests/test_conf.ini", conf);
     const char* x = get_value_ini_file(conf, "Section", "key");
     //display_all_ini_file(conf);
-    printf("%s %s(): %s\n",
-            (strcmp(x, "value") == 0) ? "PASS| " : "^^^^^FAIL| ",
-            __func__, "should parser an ini file");
+    TEST_ASSERT(strcmp(x, "value") == 0, "should parser an ini file");
     free_config(conf);
 }
 
